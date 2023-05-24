58c580c30b96fdbc05e562eeafc1d4044c4b96db
qhwkal1 <qhwkal1@naver.com>
2023-05-25 01:57:47 +0900

    0525 구독환불 모달창확인 업데이트

--- src/pages/Refund.js
+++ src/pages/Refund.js
@@ -5,6 +5,8 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import UserStore, { UserContext } from "../context/UserStore";
 import { useContext } from "react";
+import { useState } from "react";
+import Modal from "../utils/Modal";
 
 const Container = styled.div`
   display: flex;
@@ -27,22 +29,28 @@ const RePayBtn = styled.button`margin-top: 20px;
 const Refund = () => {
   // 해당 PaymentNum 갖고와서 쏴주면 끝.
   const { paymentNum } = useContext(UserContext);
+  const navigate = useNavigate(); // 환불성공시 홈으로;
+  const [showModal, setShowModal] = useState(false);
   
-  const navigate = useNavigate(); // 환불성공시 마이페이지 화면으로?
-  
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   const onClickPayBack = async() => {
-    console.log("환불")
+    console.log("환불 paymentNum : " + paymentNum);
     try {
-      // const paymentNum = 1; // 환불할 paymentNUm 긁어올 예정
       const response = await AxiosApi.payBack(paymentNum);
       const isRefund = response.data;
       if(isRefund) {
         console.log("구독권 환불 성공");
-        alert("구독권 환불이 완료되었습니다.");
-        navigate("/home");
+        alert("구독권 환불이 완료되었습니다. 더 좋은 혜택으로 보답하겠습니다.");
+        navigate("/home"); // 구독권 환불 성공시 일단 홈으로 이동
       } else {
         console.error("환불 오류");
-        navigate("/home"); // 환불실패시 어디로가야할지 ? 
+        navigate("/home"); // 환불실패시 일단 홈으로 이동 
       }
     } catch (error) {
       console.error("환불 요청 중 오류가 발생했습니다", error);
@@ -53,7 +61,16 @@ const Refund = () => {
 
   return(
     <Container>
-      <RePayBtn onClick={onClickPayBack}>환불하기</RePayBtn>
+      <RePayBtn onClick={openModal}>환불하기</RePayBtn>
+      <Modal
+        open={showModal}
+        close={closeModal}
+        header="구독권 환불 확인"
+        confirm={onClickPayBack} // 구독권 환불 로직 실행
+        type
+      >
+        <p>※ 수강기간이 종료된 강의는 환불 불가입니다. ※ 위약금 = 실제 판매금액의 10% ※ 학습수수료 및 공제수수료는 실제 수강한 강의의 단과강의 판매 가격을 기준으로 계산합니다. 정말로 환불하시겠습니까?</p>
+      </Modal>
     </Container>
   );
 };
