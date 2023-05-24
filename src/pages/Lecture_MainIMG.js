bb82cd9ced01b424794f153a40d5060be9139777
DH <lovein6114@nate.com>
2023-05-24 15:44:33 +0900

    0523 상세페이지 이미지수정

--- src/pages/Lecture_MainIMG.js
+++ src/pages/Lecture_MainIMG.js
@@ -6,14 +6,6 @@ import { storage } from "../api/firebase";
 import { ref, getDownloadURL } from "firebase/storage";
 import AxiosApi from "../api/AxiosApi";
 
-
-// 수강 설명 이미지
-const detailImg1 = <img className="detail img1" src="https://cdn.class101.net/images/aaac23bf-682b-4e06-bfaa-e0c5a1d5bf9a/2048xauto.webp" alt="그림1" />
-const detailImg2 = <img className="detail img2" src="https://cdn.class101.net/images/bd6763f2-5fe4-4e20-93b5-586ede7b4515/2048xauto.webp" alt="그림2" />
-const detailImg3 = <img className="detail img3" src="https://cdn.class101.net/images/cf838d00-61ce-440b-8ebc-cb22483fc925/960xauto.webp" alt="그림3" />
-const detailImg4 = <img className="detail img4" src="https://cdn.class101.net/images/6bedd4fb-2713-4b40-b64b-4f6e6246cbd6/960xauto.webp" alt="그림4" />
-
-
 const ClassImg = styled.div`
   /* width: auto; */
   height: 400px;
