d8cef736eaf877eae8f2d76a20db34f97a394508
qhwkal1 <qhwkal1@naver.com>
2023-05-25 01:03:35 +0900

    0525 refund 수정

--- src/pages/Refund.js
+++ src/pages/Refund.js
@@ -3,6 +3,8 @@ import React from "react";
 import AxiosApi from "../api/AxiosApi";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import UserStore, { UserContext } from "../context/UserStore";
+import { useContext } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -24,25 +26,27 @@ const RePayBtn = styled.button`margin-top: 20px;
 
 const Refund = () => {
   // 해당 PaymentNum 갖고와서 쏴주면 끝.
+  const { paymentNum } = useContext(UserContext);
   
   const navigate = useNavigate(); // 환불성공시 마이페이지 화면으로?
   
   const onClickPayBack = async() => {
     console.log("환불")
     try {
-      const paymentNum = 1; // 환불할 paymentNUm 긁어올 예정
+      // const paymentNum = 1; // 환불할 paymentNUm 긁어올 예정
       const response = await AxiosApi.payBack(paymentNum);
       const isRefund = response.data;
       if(isRefund) {
         console.log("구독권 환불 성공");
-        navigate("/MyPage");
+        alert("구독권 환불이 완료되었습니다.");
+        navigate("/home");
       } else {
         console.error("환불 오류");
-        navigate("/Mypage"); // 환불실패시 어디로가야할지 ? 
+        navigate("/home"); // 환불실패시 어디로가야할지 ? 
       }
     } catch (error) {
       console.error("환불 요청 중 오류가 발생했습니다", error);
-      navigate("/Mypage"); // 이하동문
+      navigate("/home"); // 이하동문
     };
     
   };
